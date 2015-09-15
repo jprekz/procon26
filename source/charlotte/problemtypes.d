e30a0187e384eb4081852d997c330eb80db82f76
jprekz <jprekz@users.noreply.github.com>
2015-09-15 19:06:43 +0900

    add Stone transform

--- source/charlotte/problemtypes.d
+++ source/charlotte/problemtypes.d
@@ -89,6 +89,51 @@ struct Field {
     }
 }
 
+// invert ... 反転するか否か
+// rotate(0 ~ 3) ... 引数 * 90度 右に回転
+Stone transform(Stone s, bool invert, int rotate) pure {
+	assert(rotate >= 0 && rotate < 4);
+
+    Stone stMap(string str)() {
+	    bool[8][8] output;
+    	for (int a; a < 8; a++) {
+    		for (int b; b < 8; b++) {
+                mixin(str);
+    		}
+    	}
+        return Stone(output);
+    }
+
+	if (rotate == 0) {
+        if (!invert) {
+            return stMap!("output[a][b] = s[a][b];");
+        } else {
+            return stMap!("output[a][7-b] = s[a][b];");
+        }
+	} else if (rotate == 1) {
+        if (!invert) {
+            return stMap!("output[b][7-a] = s[a][b];");
+        } else {
+            return stMap!("output[b][a] = s[a][b];");
+        }
+	} else if (rotate == 2) {
+        if (!invert) {
+            return stMap!("output[7-a][7-b] = s[a][b];");
+        } else {
+            return stMap!("output[7-a][b] = s[a][b];");
+        }
+	} else if (rotate == 3) {
+        if (!invert) {
+            return stMap!("output[7-b][a] = s[a][b];");
+        } else {
+            return stMap!("output[7-b][7-a] = s[a][b];");
+        }
+	}
+    assert(0);
+}
+
+
+
 unittest {
     assert(Stone.sizeof == 64);
     assert(Field.sizeof == 1024);
@@ -105,4 +150,8 @@ unittest {
     assert(s[1][0] == true);
     assert(s.inv.inv == s);
     assert((s & s) == s);
+    assert(s.transform(false, 0) == s);
+    assert(s.transform(true, 0).transform(true, 0) == s);
+    assert(s.transform(false, 1).transform(false, 3) == s);
+
 }
