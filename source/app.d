1768d0ced086aa327cc16f4a7ec181b5fd15aaa9
jprekz <jprekz@users.noreply.github.com>
2015-10-03 20:31:53 +0900

    meu

--- source/app.d
+++ source/app.d
@@ -10,9 +10,12 @@ import std.datetime;
 //import dfs.dfs;
 import charlotte.dfs;
 import charlotte.mc;
+import charlotte.mcts;
 
 void main(string[] args) {
-	auto solver = new MC("./practice/quest1.txt", delegate void(ans) {
+	write("input problem num: ");
+	string num = readln.chomp;
+	auto solver = new MCTS("./practice/quest"~num~".txt", delegate void(ans) {
 		File outputFile = File("./output.txt", "w");
 		foreach (string s; ans) {
 			outputFile.writeln(s);
